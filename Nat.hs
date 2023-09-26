bf0a41b8825aac083a6f4a53ba8595b4e0b443c7
Vitugome3s <victor.aguiar08@hotmail.com>
2023-09-26 12:58:30 -0300

    Sum - Mul - Exp - Fact

---
+++ Nat.hs
@@ -0,0 +1,29 @@
+module Nat where
+import Prelude hiding (sum, mult, exp, quot, min, gcd, lcm, div, max, pred, rem)
+
+
+data Nat = O | S Nat
+    deriving ( Eq , Show )
+
+-- Soma
+sum :: Nat -> Nat -> Nat
+sum n O = n
+sum n (S m) = S (sum n m)
+
+-- Multiplicação
+mul :: Nat -> Nat -> Nat
+mul n O = O
+mul n (S m) = sum (mul m n) m
+
+-- Exponenciação
+exp :: Nat -> Nat -> Nat
+exp n O = S O
+exp n (S m) = mul n (exp n m)
+
+-- Fatorial
+fact :: Nat -> Nat
+fact O = (S O)
+fact (S n) = mul (S n) (fact n)
+
+
+
